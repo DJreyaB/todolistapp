4d1586edd0d56eba1ebb5676715982094737effb
djreyab <djreya.boyd@gmail.com>
2025-02-04 12:22:43 -0600

    updated write function to accept lists w/ write lines instead of wrtie

--- functions.py
+++ functions.py
@@ -4,8 +4,8 @@ def get_todos(file_path="todos.txt"):
     return all_todos
 
 def write_todos(todos_arg, file_path="todos.txt"):
-    with open(file_path, "w") as local_file:
-        local_file.write(todos_arg)
+    with open(file_path, "w") as file:
+        file.writelines(todos_arg)
 
 
 if __name__ == "__main__":
--- gui.py
+++ gui.py
@@ -2,14 +2,28 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input = sg.InputText(tooltip="Enter todo here")
-add_button = sg.Button("Add")
+input = sg.InputText(tooltip="Enter todo here", key="todo")
+add_button = sg.Button("Add") #add an option that accepts enter
+exit_button = sg.Button("Exit")
 
 layout = [[label],
-          [input, add_button]]
+          [input, add_button],
+          [exit_button]]
 
-window = sg.Window("My Todo App", layout)
+window = sg.Window("My Todo App", layout,
+                   font=("Helvetica", 16))
+while True:
+    events, values = window.read()
+    print(events, values)
 
-window.read()
-print("Hello")
+    match events:
+        case "Add":
+            #adds todo to running list then places them in the file
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            
+            functions.write_todos(todos)
+        case "Exit" | sg.WIN_CLOSED:
+            break
 window.close()
