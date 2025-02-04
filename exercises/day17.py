1a13ab89355c5bf51090e762213bccfab3d0a195
djreyab <djreya.boyd@gmail.com>
2025-02-04 14:27:02 -0600

    completed day 17 exercise for inches to meter converter gui with full functionality

---
+++ exercises/day17.py
@@ -0,0 +1,33 @@
+# goal is to build a desktop gui program that gets feet and inches
+# as input from user and outmputs results in meters
+import PySimpleGUI as sg
+
+label1 = sg.Text("Enter feet:")
+input1 = sg.InputText()
+label2 = sg.Text("Enter inches:")
+input2 = sg.InputText()
+convert_btn = sg.Button("Convert")
+results = sg.Text('',key='meter')
+
+layout = [[label1,input1],[label2, input2],[convert_btn, results]]
+
+window = sg.Window('Converter', layout)
+
+while True:
+    events, values = window.read()
+    print(events, values)
+    match events:
+        case 'Convert':
+        #get feet and multiply by 12 then add inches answer and divide by 39.37
+            inches_total = (float(values[0]) * 12) + float(values[1])
+            print(inches_total)
+            result = inches_total / 39.37
+            print(result)
+            window['meter'].update(value=f"{result} m")
+
+            # layout = [[label1,input1],[label2, input2],[convert_btn]]
+        case sg.WIN_CLOSED:
+            break
+
+window.close()
+
